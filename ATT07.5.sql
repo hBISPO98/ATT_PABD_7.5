ae895e55b6e66bf98b3a41fb0b2e1fd49845ff88
hBISPO98 <b.hiandra@aluno.ifsp.edu.br>
2026-05-11 23:02:15 -0300

    Update ATT07.5.sql

--- ATT07.5.sql
+++ ATT07.5.sql
@@ -24,29 +24,29 @@ BEGIN
 
     -- 5. Loop para construir as faixas e contar os instrutores
     DECLARE @i INT = 0; -- contador que começa em 0
-    DECLARE @vMinAtual INT; -- Cria um espaço na memória pra guardar o v inicial de cada faixa salarial
-    DECLARE @vMaxAtual INT; -- '' valor final
+    DECLARE @vMinAtual INT; 
+    DECLARE @vMaxAtual INT;
 
-    WHILE @i < @numIntervalos -- Roda enquanto contador @i for menor que @numIntervalos
-    BEGIN -- Começo das instruções que serão repetidas
+    WHILE @i < @numIntervalos -- Roda enquanto
+    BEGIN 
         -- Cálculo dos limites da faixa atual
-        SET @vMinAtual = CAST(@minSal + (@i * @amplitude) AS INT); -- Defina (SET) o valor mínimo atual (@vMinAtual) como: pegue o salário inicial, adicione o número de 'pulos' (@i * @amplitude) que já demos e converta o resultado final para um número inteiro (INT).
-        SET @vMaxAtual = CAST(@minSal + ((@i + 1) * @amplitude) AS INT); -- '' (@vMaxAtual) como: pegue o salário inicial, adicione o próximo 'pulo' (por isso o +1) e converta o resultado final para um número inteiro.
+        SET @vMinAtual = CAST(@minSal + (@i * @amplitude) AS INT); 
+        SET @vMaxAtual = CAST(@minSal + ((@i + 1) * @amplitude) AS INT); 
         
-        -- Ajuste para não sobrepor valores (conforme o print)
+        -- Ajuste para não sobrepor valores
         IF @i > 0 SET @vMinAtual = @vMinAtual + 1; -- Soma 1 real pra não sobrepor se não for a primeira vez que esse loop roda
 
         -- Inserir na tabela de resultados contando quantos instrutores caem aqui
         INSERT INTO @HistogramaTable (valorMinimo, valorMaximo, total) -- Prepara tabela na memória pra receber os dados recém calculados
-        SELECT @vMinAtual, @vMaxAtual, COUNT(*) -- Pega os limites da fatia atual e conta quantos registros existem dentro desse espaço.
-        FROM dbo.instructor -- Indica onde a contagem deve se feita
-        WHERE salary BETWEEN @vMinAtual AND @vMaxAtual; -- Filtra salário entre (between) min e max
+        SELECT @vMinAtual, @vMaxAtual, COUNT(*) 
+        FROM dbo.instructor 
+        WHERE salary BETWEEN @vMinAtual AND @vMaxAtual; 
 
         SET @i = @i + 1; -- Soma 1 ao contador @i pra que o código entenda que deve passar para a próxima faixa 
-    END -- Marca o fim do bloco de repetição
+    END 
 
-    -- 6. Exibir o resultado final ordenado
-    SELECT * FROM @HistogramaTable ORDER BY valorMinimo; -- Resultados do min para o max
+    -- 6. Exibir o resultado final ordenado de min para max
+    SELECT * FROM @HistogramaTable ORDER BY valorMinimo; 
 END;
 GO
 
